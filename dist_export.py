d8526d5b35d651b01c8203faf087f0ac410e1016
zhaopei <zhaopei0418@aol.com>
2019-12-05 12:50:38 +0800

    添加导出入出区核放单订单模板

--- dist_export.py
+++ dist_export.py
@@ -1,9 +1,11 @@
 import os
 import cx_Oracle
+import redis
 import traceback
 import xlsxwriter
 from flask import (
-    Flask, request, render_template, Blueprint,
+    Flask,
+    Blueprint,
     send_from_directory
 )
 from datetime import datetime
@@ -16,16 +18,23 @@ bpout = Blueprint('out', __name__, url_prefix='/maintain/distOut', static_folder
 username = os.getenv('ORCL_USERNAME') or 'username'
 password = os.getenv('ORCL_PASSWORD') or 'password'
 dbUrl = os.getenv('ORCL_DBURL') or '127.0.0.1:1521/orcl'
+redisHost = os.getenv('REDIS_HOST') or '127.0.0.1'
+redisPort = os.getenv('REDIS_PORT') or 6379
 
+
+redisPool = redis.ConnectionPool(host=redisHost, port=redisPort)
+redisCon = redis.Redis(connection_pool=redisPool)
 
 def executeSql(sql, **kw):
     con = cx_Oracle.connect(username, password, dbUrl)
     cursor = con.cursor()
     result = None
     try:
+        now = datetime.now()
         cursor.prepare(sql)
         cursor.execute(None, kw)
         result = cursor.fetchall()
+        print('time: [{}] execute sql: [{}] para: [{}] result count: [{}]'.format(now.strftime('%Y-%m-%d %H:%M:%S'), sql, kw, len(result)))
         con.commit()
     except Exception:
         traceback.print_exc()
@@ -107,7 +116,7 @@ def inExport(distno):
 
 
 @bpout.route('/export/<distno>', methods=['GET'])
-def inExport(distno):
+def outExport(distno):
     now = datetime.now()
     xlsxDir = "export"
     fileName = "{}_{}_out.xlsx".format(distno, now.strftime('%Y%m%d%H%M%S'))
@@ -175,6 +184,143 @@ def inExport(distno):
     wb.close()
     return send_from_directory(xlsxDir, fileName, as_attachment=True)
 
+
+@bpout.route('/exportin/<token>/<billNo>', methods=['GET'])
+def outExportInOrderTemplate(token, billNo):
+    now = datetime.now()
+    xlsxDir = "export"
+    fileName = "{}_{}_order_in.xlsx".format(billNo, now.strftime('%Y%m%d%H%M%S'))
+    print("fileName is: {}".format(fileName))
+    wb = xlsxwriter.Workbook(os.path.join(xlsxDir, fileName))
+
+    companyCode = redisCon.get(token).decode()
+
+    sht1 = wb.add_worksheet('Sheet1')
+    sht1.write_string(0, 0, '订单编号')
+    sht1.write_string(0, 1, '电商平台代码')
+
+    sht2 = wb.add_worksheet('未下发')
+    sht2.write_string(0, 0, '订单编号')
+    sht2.write_string(0, 1, '电商平台代码')
+    sht2.write_string(0, 2, '运单号')
+    sht2.write_string(0, 3, '外网状态')
+
+    if companyCode is None:
+        sht1.write_string(1, 0, 'token不存在，请联系管理员，申请开通')
+        wb.close()
+        return send_from_directory(xlsxDir, fileName, as_attachment=True)
+
+    sql = '''
+    select t.order_no, t.ebp_code
+    from ceb3_invt_head t
+    where t.app_status in ('399', '500', '800')
+    and t.bill_no = :billNo
+    and t.cus_status is not null
+    and (t.ebc_code = :ebcCode
+    or t.ebp_code = :ebpCode
+    or t.logistics_code = :logisticsCode
+    or t.agent_code = :agentCode)
+    '''
+    result = executeSql(sql, billNo=billNo, ebcCode=companyCode, ebpCode=companyCode, logisticsCode=companyCode, agentCode=companyCode)
+
+    row = 1
+    for invt in result:
+        sht1.write_string(row, 0, invt[0])
+        sht1.write_string(row, 1, invt[1])
+        row = row + 1
+
+    sql = '''
+    select t.order_no, t.ebp_code, t.logistics_no, t.app_status
+    from ceb3_invt_head t
+    where t.bill_no = :billNo
+    and t.cus_status is null
+    and (t.ebc_code = :ebcCode
+    or t.ebp_code = :ebpCode
+    or t.logistics_code = :logisticsCode
+    or t.agent_code = :agentCode)
+    '''
+    result = executeSql(sql, billNo=billNo, ebcCode=companyCode, ebpCode=companyCode, logisticsCode=companyCode, agentCode=companyCode)
+
+    row = 1
+    for invt in result:
+        sht2.write_string(row, 0, invt[0])
+        sht2.write_string(row, 1, invt[1])
+        sht2.write_string(row, 2, invt[2])
+        sht2.write_string(row, 3, invt[3])
+        row = row + 1
+
+    wb.close()
+    return send_from_directory(xlsxDir, fileName, as_attachment=True)
+
+
+@bpout.route('/exportout/<token>/<billNo>', methods=['GET'])
+def outExportOutOrderTemplate(token, billNo):
+    now = datetime.now()
+    xlsxDir = "export"
+    fileName = "{}_{}_order_out.xlsx".format(billNo, now.strftime('%Y%m%d%H%M%S'))
+    print("fileName is: {}".format(fileName))
+    wb = xlsxwriter.Workbook(os.path.join(xlsxDir, fileName))
+
+    companyCode = redisCon.get(token).decode()
+
+    sht1 = wb.add_worksheet('Sheet1')
+    sht1.write_string(0, 0, '订单编号')
+    sht1.write_string(0, 1, '电商平台代码')
+
+    sht2 = wb.add_worksheet('未放行或者未下发')
+    sht2.write_string(0, 0, '订单编号')
+    sht2.write_string(0, 1, '电商平台代码')
+    sht2.write_string(0, 2, '运单号')
+    sht2.write_string(0, 3, '外网状态')
+    sht2.write_string(0, 4, '内网状态')
+
+    if companyCode is None:
+        sht1.write_string(1, 0, 'token不存在，请联系管理员，申请开通')
+        wb.close()
+        return send_from_directory(xlsxDir, fileName, as_attachment=True)
+
+    sql = '''
+    select t.order_no, t.ebp_code
+    from ceb3_invt_head t
+    where t.app_status = '800'
+    and t.bill_no = :billNo
+    and t.cus_status = '26'
+    and (t.ebc_code = :ebcCode
+    or t.ebp_code = :ebpCode
+    or t.logistics_code = :logisticsCode
+    or t.agent_code = :agentCode)
+    '''
+    result = executeSql(sql, billNo=billNo, ebcCode=companyCode, ebpCode=companyCode, logisticsCode=companyCode, agentCode=companyCode)
+
+    row = 1
+    for invt in result:
+        sht1.write_string(row, 0, invt[0])
+        sht1.write_string(row, 1, invt[1])
+        row = row + 1
+
+    sql = '''
+    select t.order_no, t.ebp_code, t.logistics_no, t.app_status, t.cus_status
+    from ceb3_invt_head t
+    where t.bill_no = :billNo
+    and (t.cus_status != '26' or t.cus_status is null)
+    and (t.ebc_code = :ebcCode
+    or t.ebp_code = :ebpCode
+    or t.logistics_code = :logisticsCode
+    or t.agent_code = :agentCode)
+    '''
+    result = executeSql(sql, billNo=billNo, ebcCode=companyCode, ebpCode=companyCode, logisticsCode=companyCode, agentCode=companyCode)
+
+    row = 1
+    for invt in result:
+        sht2.write_string(row, 0, invt[0])
+        sht2.write_string(row, 1, invt[1])
+        sht2.write_string(row, 2, invt[2])
+        sht2.write_string(row, 3, invt[3])
+        sht2.write_string(row, 4, invt[4] or '未下发')
+        row = row + 1
+
+    wb.close()
+    return send_from_directory(xlsxDir, fileName, as_attachment=True)
 
 app.register_blueprint(bpin)
 app.register_blueprint(bpout)
